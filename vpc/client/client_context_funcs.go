dd7b7a6eefe50007c2c610b2e43d42d7bcc2c57e
vardhaman22 <vardhaman.surana@suse.com>
2025-09-03 07:35:12 +0530

    added vpc client

---
+++ vpc/client/client_context_funcs.go
@@ -0,0 +1,220 @@
+package client
+
+import (
+	"context"
+
+	openapiutil "github.com/rancher/muchang/darabonba-openapi/utils"
+	"github.com/rancher/muchang/utils/tea/dara"
+)
+
+// Summary:
+//
+// Queries virtual private clouds (VPCs).
+//
+// @param request - DescribeVpcsRequest
+//
+// @param runtime - runtime options for this request RuntimeOptions
+//
+// @return DescribeVpcsResponse
+func (client *Client) DescribeVpcsWithContext(ctx context.Context, request *DescribeVpcsRequest, runtime *dara.RuntimeOptions) (_result *DescribeVpcsResponse, _err error) {
+	_err = request.Validate()
+	if _err != nil {
+		return _result, _err
+	}
+	query := map[string]interface{}{}
+	if !dara.IsNil(request.DhcpOptionsSetId) {
+		query["DhcpOptionsSetId"] = request.DhcpOptionsSetId
+	}
+
+	if !dara.IsNil(request.DryRun) {
+		query["DryRun"] = request.DryRun
+	}
+
+	if !dara.IsNil(request.EnableIpv6) {
+		query["EnableIpv6"] = request.EnableIpv6
+	}
+
+	if !dara.IsNil(request.IsDefault) {
+		query["IsDefault"] = request.IsDefault
+	}
+
+	if !dara.IsNil(request.OwnerAccount) {
+		query["OwnerAccount"] = request.OwnerAccount
+	}
+
+	if !dara.IsNil(request.OwnerId) {
+		query["OwnerId"] = request.OwnerId
+	}
+
+	if !dara.IsNil(request.PageNumber) {
+		query["PageNumber"] = request.PageNumber
+	}
+
+	if !dara.IsNil(request.PageSize) {
+		query["PageSize"] = request.PageSize
+	}
+
+	if !dara.IsNil(request.RegionId) {
+		query["RegionId"] = request.RegionId
+	}
+
+	if !dara.IsNil(request.ResourceGroupId) {
+		query["ResourceGroupId"] = request.ResourceGroupId
+	}
+
+	if !dara.IsNil(request.ResourceOwnerAccount) {
+		query["ResourceOwnerAccount"] = request.ResourceOwnerAccount
+	}
+
+	if !dara.IsNil(request.ResourceOwnerId) {
+		query["ResourceOwnerId"] = request.ResourceOwnerId
+	}
+
+	if !dara.IsNil(request.Tag) {
+		query["Tag"] = request.Tag
+	}
+
+	if !dara.IsNil(request.VpcId) {
+		query["VpcId"] = request.VpcId
+	}
+
+	if !dara.IsNil(request.VpcName) {
+		query["VpcName"] = request.VpcName
+	}
+
+	if !dara.IsNil(request.VpcOwnerId) {
+		query["VpcOwnerId"] = request.VpcOwnerId
+	}
+
+	req := &openapiutil.OpenApiRequest{
+		Query: openapiutil.Query(query),
+	}
+	params := &openapiutil.Params{
+		Action:      dara.String("DescribeVpcs"),
+		Version:     dara.String("2016-04-28"),
+		Protocol:    dara.String("HTTPS"),
+		Pathname:    dara.String("/"),
+		Method:      dara.String("POST"),
+		AuthType:    dara.String("AK"),
+		Style:       dara.String("RPC"),
+		ReqBodyType: dara.String("formData"),
+		BodyType:    dara.String("json"),
+	}
+	_result = &DescribeVpcsResponse{}
+	_body, _err := client.CallApiWithCtx(ctx, params, req, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = dara.Convert(_body, &_result)
+	return _result, _err
+}
+
+// Summary:
+//
+// Queries the information about available vSwitches that are used for an internal network.
+//
+// @param request - DescribeVSwitchesRequest
+//
+// @param runtime - runtime options for this request RuntimeOptions
+//
+// @return DescribeVSwitchesResponse
+func (client *Client) DescribeVSwitchesWithContext(ctx context.Context, request *DescribeVSwitchesRequest, runtime *dara.RuntimeOptions) (_result *DescribeVSwitchesResponse, _err error) {
+	_err = request.Validate()
+	if _err != nil {
+		return _result, _err
+	}
+	query := map[string]interface{}{}
+	if !dara.IsNil(request.DryRun) {
+		query["DryRun"] = request.DryRun
+	}
+
+	if !dara.IsNil(request.EnableIpv6) {
+		query["EnableIpv6"] = request.EnableIpv6
+	}
+
+	if !dara.IsNil(request.IsDefault) {
+		query["IsDefault"] = request.IsDefault
+	}
+
+	if !dara.IsNil(request.OwnerAccount) {
+		query["OwnerAccount"] = request.OwnerAccount
+	}
+
+	if !dara.IsNil(request.OwnerId) {
+		query["OwnerId"] = request.OwnerId
+	}
+
+	if !dara.IsNil(request.PageNumber) {
+		query["PageNumber"] = request.PageNumber
+	}
+
+	if !dara.IsNil(request.PageSize) {
+		query["PageSize"] = request.PageSize
+	}
+
+	if !dara.IsNil(request.RegionId) {
+		query["RegionId"] = request.RegionId
+	}
+
+	if !dara.IsNil(request.ResourceGroupId) {
+		query["ResourceGroupId"] = request.ResourceGroupId
+	}
+
+	if !dara.IsNil(request.ResourceOwnerAccount) {
+		query["ResourceOwnerAccount"] = request.ResourceOwnerAccount
+	}
+
+	if !dara.IsNil(request.ResourceOwnerId) {
+		query["ResourceOwnerId"] = request.ResourceOwnerId
+	}
+
+	if !dara.IsNil(request.RouteTableId) {
+		query["RouteTableId"] = request.RouteTableId
+	}
+
+	if !dara.IsNil(request.Tag) {
+		query["Tag"] = request.Tag
+	}
+
+	if !dara.IsNil(request.VSwitchId) {
+		query["VSwitchId"] = request.VSwitchId
+	}
+
+	if !dara.IsNil(request.VSwitchName) {
+		query["VSwitchName"] = request.VSwitchName
+	}
+
+	if !dara.IsNil(request.VSwitchOwnerId) {
+		query["VSwitchOwnerId"] = request.VSwitchOwnerId
+	}
+
+	if !dara.IsNil(request.VpcId) {
+		query["VpcId"] = request.VpcId
+	}
+
+	if !dara.IsNil(request.ZoneId) {
+		query["ZoneId"] = request.ZoneId
+	}
+
+	req := &openapiutil.OpenApiRequest{
+		Query: openapiutil.Query(query),
+	}
+	params := &openapiutil.Params{
+		Action:      dara.String("DescribeVSwitches"),
+		Version:     dara.String("2016-04-28"),
+		Protocol:    dara.String("HTTPS"),
+		Pathname:    dara.String("/"),
+		Method:      dara.String("POST"),
+		AuthType:    dara.String("AK"),
+		Style:       dara.String("RPC"),
+		ReqBodyType: dara.String("formData"),
+		BodyType:    dara.String("json"),
+	}
+	_result = &DescribeVSwitchesResponse{}
+	_body, _err := client.CallApiWithCtx(ctx, params, req, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = dara.Convert(_body, &_result)
+	return _result, _err
+}
---
+++ vpc/client/describe_vpcs_response_model.go
@@ -0,0 +1,62 @@
+package client
+
+import (
+	"github.com/rancher/muchang/utils/tea/dara"
+)
+
+type iDescribeVpcsResponse interface {
+	dara.Model
+	String() string
+	GoString() string
+	SetHeaders(v map[string]*string) *DescribeVpcsResponse
+	GetHeaders() map[string]*string
+	SetStatusCode(v int32) *DescribeVpcsResponse
+	GetStatusCode() *int32
+	SetBody(v *DescribeVpcsResponseBody) *DescribeVpcsResponse
+	GetBody() *DescribeVpcsResponseBody
+}
+
+type DescribeVpcsResponse struct {
+	Headers    map[string]*string        `json:"headers,omitempty" xml:"headers,omitempty"`
+	StatusCode *int32                    `json:"statusCode,omitempty" xml:"statusCode,omitempty"`
+	Body       *DescribeVpcsResponseBody `json:"body,omitempty" xml:"body,omitempty"`
+}
+
+func (s DescribeVpcsResponse) String() string {
+	return dara.Prettify(s)
+}
+
+func (s DescribeVpcsResponse) GoString() string {
+	return s.String()
+}
+
+func (s *DescribeVpcsResponse) GetHeaders() map[string]*string {
+	return s.Headers
+}
+
+func (s *DescribeVpcsResponse) GetStatusCode() *int32 {
+	return s.StatusCode
+}
+
+func (s *DescribeVpcsResponse) GetBody() *DescribeVpcsResponseBody {
+	return s.Body
+}
+
+func (s *DescribeVpcsResponse) SetHeaders(v map[string]*string) *DescribeVpcsResponse {
+	s.Headers = v
+	return s
+}
+
+func (s *DescribeVpcsResponse) SetStatusCode(v int32) *DescribeVpcsResponse {
+	s.StatusCode = &v
+	return s
+}
+
+func (s *DescribeVpcsResponse) SetBody(v *DescribeVpcsResponseBody) *DescribeVpcsResponse {
+	s.Body = v
+	return s
+}
+
+func (s *DescribeVpcsResponse) Validate() error {
+	return dara.Validate(s)
+}
